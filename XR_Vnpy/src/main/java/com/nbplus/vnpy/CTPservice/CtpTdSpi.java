29a52dae642f2d9e8b747d1d6060196215a9507e
gongtan <13279429701@163.com>
2019-11-25 12:29:22 +0800

    更新

--- XR_Vnpy/src/main/java/com/nbplus/vnpy/CTPservice/CtpTdSpi.java
+++ XR_Vnpy/src/main/java/com/nbplus/vnpy/CTPservice/CtpTdSpi.java
@@ -4,6 +4,7 @@ import com.nbplus.vnpy.CTPservice.impl.TraderSpiImpl;
 import ctp.thostmduserapi.CThostFtdcMdSpi;
 import ctp.thosttraderapi.*;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.Resource;
 
 /**
@@ -12,11 +13,10 @@ import javax.annotation.Resource;
  * @Date 2019/11/21 16:18
  * @Version 1.0
  **/
- class CtpTdSpi extends CThostFtdcTraderSpi {
+class CtpTdSpi extends CThostFtdcTraderSpi {
 
 
     private CThostFtdcTraderApi m_traderapi;
-
 
 
     final static String m_BrokerId = "9999";
@@ -36,16 +36,15 @@ import javax.annotation.Resource;
     private boolean loginFailed; // 登录失败（账号密码错误）*/
 
 
-   CtpTdSpi(CThostFtdcTraderApi traderapi){
+    CtpTdSpi(CThostFtdcTraderApi traderapi) {
         m_traderapi = traderapi;
     }
-
 
 
     // 当客户端与交易托管系统建立起通信连接，客户端需要进行登录
     //连接前 所执行的 方法
     @Override
-    public void OnFrontConnected(){
+    public void OnFrontConnected() {
         System.out.println("On Front Connected");
         CThostFtdcReqAuthenticateField field = new CThostFtdcReqAuthenticateField();
         field.setBrokerID(m_BrokerId);
@@ -63,20 +62,51 @@ import javax.annotation.Resource;
 
     // 当客户端发出登录请求之后，该方法会被调用，通知客户端登录是否成功
     @Override
-    public void OnRspUserLogin(CThostFtdcRspUserLoginField cThostFtdcRspUserLoginField, CThostFtdcRspInfoField pRspInfo, int i, boolean b) {
-        if(pRspInfo != null && pRspInfo.getErrorID() != 0){
+    public void OnRspUserLogin(CThostFtdcRspUserLoginField pRspUserLogin, CThostFtdcRspInfoField pRspInfo, int nRequestID, boolean bIsLast) {
+        if (pRspInfo != null && pRspInfo.getErrorID() != 0) {
             //登录失败的  打印错误提示
             System.out.printf("Login ErrorID[%d] ErrMsg[%s]\n", pRspInfo.getErrorID(), pRspInfo.getErrorMsg());
-            // 下面不会执行 return 跳出方法
             return;
         }
         //登录成功
         System.out.println("Login success!!!");
+
+        //请求查询资金账户
+        CThostFtdcQryTradingAccountField qryTradingAccount = new CThostFtdcQryTradingAccountField();
+        qryTradingAccount.setBrokerID(m_BrokerId);
+        qryTradingAccount.setCurrencyID(m_CurrencyId);
+        qryTradingAccount.setInvestorID(m_InvestorId);
+        //m_traderapi.ReqQryTradingAccount(qryTradingAccount, 1);
+
+        //请求查询投资者结算结果
+        CThostFtdcQrySettlementInfoField qrysettlement = new CThostFtdcQrySettlementInfoField();
+        qrysettlement.setBrokerID(m_BrokerId);
+        qrysettlement.setInvestorID(m_InvestorId);
+        qrysettlement.setTradingDay(m_TradingDay);
+        qrysettlement.setAccountID(m_AccountId);
+        qrysettlement.setCurrencyID(m_CurrencyId);
+        m_traderapi.ReqQrySettlementInfo(qrysettlement, 2);
+        System.out.println("Query success!!!");
     }
 
     @Override
-    public void OnRspAuthenticate(CThostFtdcRspAuthenticateField pRspAuthenticateField, CThostFtdcRspInfoField pRspInfo, int nRequestID, boolean bIsLast)
-    {
+    public void OnRspAuthenticate(CThostFtdcRspAuthenticateField pRspAuthenticateField, CThostFtdcRspInfoField pRspInfo, int nRequestID, boolean bIsLast) {
+        System.out.println(pRspInfo.getErrorID());
+        if (pRspInfo != null && pRspInfo.getErrorID() != 0) {
+            System.out.printf("Login ErrorID[%d] ErrMsg[%s]\n", pRspInfo.getErrorID(), pRspInfo.getErrorMsg());
 
+            return;
+        }
+        System.out.println("OnRspAuthenticate success!!!");
+        CThostFtdcReqUserLoginField field = new CThostFtdcReqUserLoginField();
+        field.setBrokerID(m_BrokerId);
+        field.setUserID(m_UserId);
+        field.setPassword(m_PassWord);
+        m_traderapi.ReqUserLogin(field, 0);
+        System.out.println("Send login ok");
     }
+
+
+
+
 }
